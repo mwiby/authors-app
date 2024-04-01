decf0ae381281537b59bcd15e53575a50cd80600
Markus Byremo <markuswb77@gmail.com>
2024-04-01 09:38:05 +0200

    refactor on route

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,12 @@ const router = createRouter({
       component: PostView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/authors',
+      name: 'authors',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AuthorsView.vue')
     }
   ]
 })
