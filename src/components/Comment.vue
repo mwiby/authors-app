631b60f88f608cdfd185fb0c66b8344a29f17173
Markus Byremo <markuswb77@gmail.com>
2024-04-02 08:24:58 +0200

    comment component style

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -4,10 +4,32 @@ defineProps(['comments'])
 </script>
 
 <template>
-  <div>
-    <div v-for="comment in comments" :key="comment.id">
-      <h3>{{ comment.name }}</h3>
-      <p>{{ comment.body }}</p>
+  <div class="comment-container">
+    <div v-for="comment in comments" :key="comment.id" class="comment">
+      <h3 class="comment-name">{{ comment.name }}</h3>
+      <p class="comment-body">{{ comment.body }}</p>
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.comment-container {
+  margin-top: 1rem;
+}
+
+.comment {
+  background-color: #f4f4f4;
+  padding: 1rem;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+
+.comment-name {
+  margin-bottom: 0.5rem;
+  color: #333;
+}
+
+.comment-body {
+  color: #666;
+}
+</style>
