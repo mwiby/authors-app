b162934f430f83335c09e81338d18858baaad570
Markus Byremo <markuswb77@gmail.com>
2024-04-02 08:18:34 +0200

    Author component styled

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -6,11 +6,29 @@ defineProps(['author', 'posts'])
 </script>
 
 <template>
-  <div>
+  <div class="author-container">
     <h1>{{ author.name }}</h1>
     <p>{{ posts.length }} posts written.</p>
-    <p v-for="post in posts" :key="post.id">
-      <RouterLink :to="`/post/${post.id}`">{{ post.title }}</RouterLink>
-    </p>
+    <div v-for="post in posts" :key="post.id" class="post">
+      <RouterLink :to="`/post/${post.id}`" class="post-link">{{ post.title }}</RouterLink>
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.author-container {
+  padding: 1rem;
+}
+
+.post {
+  margin-top: 1rem;
+}
+
+.post-link {
+  color: #000;
+
+  &:hover {
+    color: #e2b121;
+  }
+}
+</style>
