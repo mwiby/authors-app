228fa0e0406f9a131c1728d3f1e6c44758e8865c
Markus Byremo <markuswb77@gmail.com>
2024-04-04 17:37:42 +0200

    removed boilerplate

--- src/stores/post.js
+++ src/stores/post.js
@@ -1,16 +1,5 @@
 import { defineStore } from 'pinia'
 
-/* 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
-*/
 export const usePostStore = defineStore({
   id: 'post',
   state: () => ({
