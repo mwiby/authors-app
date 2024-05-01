6acc2a3c50f8706b37bdfd49a8e1d9ddc56b09f2
Markus Byremo <markuswb77@gmail.com>
2024-05-01 17:11:30 +0200

    small style adjustment on author view

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -6,22 +6,36 @@ defineProps(['author', 'posts'])
 </script>
 
 <template>
-  <div class="author-container">
+  <div class="container">
     <h1>{{ author.name }}</h1>
     <p>{{ posts.length }} posts written.</p>
-    <div v-for="post in posts" :key="post.id" class="post">
-      <RouterLink :to="`/post/${post.id}`" class="post-link">{{ post.title }}</RouterLink>
+    <div class="author-container">
+      <div v-for="post in posts" :key="post.id" class="post-card">
+        <div class="post">
+          <RouterLink :to="`/post/${post.id}`" class="post-link">{{ post.title }}</RouterLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.container {
+  padding: 5px 2px;
+}
 .author-container {
-  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 1rem;
+  padding: 0.2rem;
 }
 
-.post {
+.post-card {
+  background-color: #f4f4f4;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   margin-top: 1rem;
+  padding: 1rem;
 }
 
 .post-link {
